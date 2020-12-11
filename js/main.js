ce9328ca8a2113ac32c58b73a96b1175b9edd302
matheus-debug <matheussantanaengineer@gmail.com>
2020-12-10 21:23:21 -0300

    Aula 2

--- js/main.js
+++ js/main.js
@@ -13,11 +13,15 @@ var frase = 'Japão é o melhor time do mundo';
 console.log(n1 + n2, 'É o resultado da soma'); //Para fazer operações aritiméticas não podemos colocar apostrofe ou aspas nos valores das variaveis
 console.log(frase.replace('Japão', 'Brasil')); //.replace realiza a substituição do valor de uma string por outra*/
 
-// AULA 2: ARRAY E DICIONÁRIO
+// *************AULA 2: ARRAY E DICIONÁRIO*****************
 
 var lista = ['Maça', 'Pêra', 'Morango']
 //lista.push('Uva'); // .push é utilizado para adicionar valores para um array existente
 //lista.pop(); //remove o ultimo valor do array
 console.log(lista.length); // .length exibe a quantidade de itens na variavel
 console.log(lista.reverse()) // .reverse() retorna os valores do array do ultimo para o primeiro
+console.log(lista.toString()) // Transforma os valores do array em strings
+console.log(lista.join(' || ')) // Tranforma os valores do array em string, porem vc pode selecionar a formatação da separação dos itens
 
+var fruta = {nome: 'Maça', cor:'Vermelho'}
+console.log(fruta);
